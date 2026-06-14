bd0de12ad659e4b3af0ddcab2db701823756bbf7
Hamadaaahmed <mm01063903831@gmail.com>
2026-06-14 17:42:27 +0300

    my local changes

--- lib/features/auth/presentation/screens/auth_gate_screen.dart
+++ lib/features/auth/presentation/screens/auth_gate_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 import '../../../../app/router.dart';
 import '../../../../core/secure_storage.dart';
-import '../../data/auth_service.dart';
 
 class AuthGateScreen extends StatefulWidget {
   const AuthGateScreen({super.key});
@@ -12,6 +10,7 @@ class AuthGateScreen extends StatefulWidget {
 }
 
 class _AuthGateScreenState extends State<AuthGateScreen> {
+
   @override
   void initState() {
     super.initState();
@@ -19,43 +18,21 @@ class _AuthGateScreenState extends State<AuthGateScreen> {
   }
 
   Future<void> _boot() async {
-    final adminToken = await AppStorage.I.getAdminToken();
-    final clientToken = await AppStorage.I.getClientToken();
-    final clientEmail = await AppStorage.I.getClientEmail();
-    bool clientPhoneGatePending =
-        await AppStorage.I.isClientPhoneGatePending();
 
-    final hasAdminToken = adminToken != null && adminToken.isNotEmpty;
-    final hasClientToken = clientToken != null && clientToken.isNotEmpty;
-    final hasClientEmail = clientEmail != null && clientEmail.trim().isNotEmpty;
-
-    if (hasClientToken && hasClientEmail && clientPhoneGatePending) {
-      try {
-        final phoneStatus =
-            await AuthService().getPhoneStatus(clientEmail.trim().toLowerCase());
-
-        if (phoneStatus.ok && phoneStatus.hasPhone) {
-          await AppStorage.I.clearClientPhoneGatePending();
-          clientPhoneGatePending = false;
-        }
-      } catch (_) {}
-    }
+    final token = await SecureStorage.I.read('token');
+    final role = await SecureStorage.I.read('role');
 
     if (!mounted) return;
 
-    final nextRoute = hasAdminToken
-        ? AppRouter.adminHome
-        : (hasClientToken
-            ? ((clientPhoneGatePending && hasClientEmail)
-                ? AppRouter.clientPhoneGate
-                : AppRouter.clientHome)
-            : AppRouter.clientEmail);
+    String nextRoute = AppRouter.clientEmail;
 
-    Navigator.pushReplacementNamed(
-      context,
-      nextRoute,
-      arguments: nextRoute == AppRouter.clientPhoneGate ? clientEmail : null,
-    );
+    if (token != null && token.isNotEmpty) {
+      nextRoute = (role == 'admin')
+          ? AppRouter.adminHome
+          : AppRouter.clientHome;
+    }
+
+    Navigator.pushReplacementNamed(context, nextRoute);
   }
 
   @override
